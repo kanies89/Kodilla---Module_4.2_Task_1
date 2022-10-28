98853748966706598c2222dd6e51de8be2fcdd9d
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2022-10-28 15:48:33 +0200

    main.py - finished task

---
+++ Module_4.2_Task_1/main.py
@@ -0,0 +1,37 @@
+"""
+Zadanie: palindromy
+Pamiętaj, że wszystkie zadania, które wysyłasz Mentorowi powinny być umieszczone w Twoim zdalnym repozytorium,
+jako osobne projekty. W czasie pracy zapisuj więc kolejne commity i prześlij całość na serwer w serwisie GitHub.
+
+Twoim zadaniem będzie napisanie funkcji, która sprawdza, czy dany wyraz jest palindromem. Palindrom to słowo,
+które czytane od lewej do prawej i od prawej do lewej brzmi tak samo. Przykłady to “kajak” i “potop”.
+
+Zaprogramuj funkcję, która przyjmuje jeden argument (typu string) i zwraca wartość boolean: True/False,
+mówiącą czy podany tekst jest palindromem.
+
+Podpowiedź
+Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji,
+które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
+
+Do zadania dodaj krótką dokumentację i umieść je w zdalnym repozytorium. Link prześlij Mentorowi.
+"""
+
+def is_palindrome(word):
+    """
+    Function which checks if word is palindrome.
+
+    :param word: checks if given string is palindrome
+    :return: True / False
+    """
+    length = len(word) + 1
+    is_word_palindrome = ''
+    for i in range(-1, -length, -1):
+        is_word_palindrome += word[i]
+    if word == is_word_palindrome:
+        return True
+    else:
+        return False
+
+print(is_palindrome('kajak'))
+print(is_palindrome('potop'))
+print(is_palindrome('epopeja'))
